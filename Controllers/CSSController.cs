760072422ec26b4dd2302f0cb997d430af5b9831
hircjusz <darek.uchman@gmail.com>
2015-09-10 22:43:42 +0200

    css workout

---
+++ Controllers/CSSController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExtJsPlugins.Controllers
+{
+    public class CSSController : Controller
+    {
+        //
+        // GET: /CSS/
+
+        public ActionResult Index()
+        {
+            return View("Navigation");
+        }
+
+    }
+}
