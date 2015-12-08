056f2445f6db473687f444d49424cd5bcce634cb
hircjusz <darek.uchman@gmail.com>
2015-12-08 23:35:39 +0100

    component+comboboxes

--- Scripts/plugins/customComponents.js
+++ Scripts/plugins/customComponents.js
@@ -134,25 +134,25 @@
     var myPluginReadonlyField = function () {
 
         Ext.define('Ext.ux.ReadOnlyField', {
-            alias:'plugin.readonlybutton',
-            init: function(parent) {
+            alias: 'plugin.readonlybutton',
+            init: function (parent) {
                 this.parent = parent;
                 this.initEventHandlers();
                 this.parent.save = this.save;
             },
-            save: function() {
-                 if (this.rendered) {
-                     this.displayEl.update(this.getValue());
-                     this.displayEl.show();
-                     this.inputEl.hide();
-                 }
+            save: function () {
+                if (this.rendered) {
+                    this.displayEl.update(this.getValue());
+                    this.displayEl.show();
+                    this.inputEl.hide();
+                }
             },
-            initEventHandlers: function() {
-                 this.parent.on({
-                     render: this.onParentRender,
-                     scope: this
-                 });
-            }, onParentRender: function(field) {
+            initEventHandlers: function () {
+                this.parent.on({
+                    render: this.onParentRender,
+                    scope: this
+                });
+            }, onParentRender: function (field) {
                 field.displayEl =
                     Ext.DomHelper.append(field.bodyEl, {
                         tag: 'div',
@@ -174,7 +174,7 @@
                     {
                         xtype: 'button',
                         text: 'Save',
-                        handler: function() {
+                        handler: function () {
                             form.items.get(0).save();
                         }
                     },
@@ -186,7 +186,281 @@
                 }]
             });
     };
-    myPluginReadonlyField();
 
 
+    var mySubmitButton = function () {
+
+        Ext.define('ns.component.Submit', {
+            extend: 'Ext.Component',
+            renderTo: document.body,
+            autoEl: { tag: 'input', cls: 'custom_loginbtn', type: 'submit', value: 'Submit' },
+            listeners: {
+                afterrender: function (inputCmp) {
+                    inputCmp.mon(inputCmp.el, 'click', function () {
+                        alert('click');
+                    });
+                }
+            }
+
+        });
+
+        var el = Ext.create('ns.component.Submit');
+
+    };
+
+
+    var viewImage = function () {
+
+        Ext.define('ns.Image', {
+            extend: 'Ext.data.Model',
+            fields: [
+                { name: 'src', type: 'string' },
+                { name: 'caption', type: 'string' }
+            ]
+        }
+            );
+
+       var store=  Ext.create('Ext.data.Store', {
+            id: 'imagesStore',
+            model: 'ns.Image',
+            data: [
+                 { src: 'http://www.sencha.com/img/20110215-feat-drawing.png', caption: 'Drawing & Charts' },
+        { src: 'http://www.sencha.com/img/20110215-feat-data.png', caption: 'Advanced Data' },
+        { src: 'http://www.sencha.com/img/20110215-feat-html5.png', caption: 'Overhauled Theme' },
+        { src: 'http://www.sencha.com/img/20110215-feat-perf.png', caption: 'Performance Tuned' }
+            ]
+       });
+
+       Ext.util.Observable.capture(store, function (evname) { console.log(evname, arguments); });
+
+
+       var searchedStore = Ext.data.StoreManager.lookup('imagesStore');
+
+        var imageTpl = new Ext.XTemplate(
+                    '<tpl for=".">',
+                      '<div style="margin: 20px; border:1px solid black" class="thumb-wrap">',
+                        '<img src="{src}" />',
+                        '<br/><span>{caption}</span>',
+                      '</div>',
+                    '</tpl>'
+                );
+
+        var itemContextMenu = Ext.create('Ext.menu.Menu', {
+            renderTo: Ext.getBody(),
+            items: [{
+                text: 'item context menu'
+            }]
+        });
+
+        var containerContextMenu = Ext.create('Ext.menu.Menu', {
+            renderTo: Ext.getBody(),
+            items: [{
+                text: 'container context menu'
+            }]
+        });
+
+        var view = Ext.create('Ext.view.View', {
+            store: searchedStore,
+            tpl: imageTpl,
+            imageSelector: 'div.thum-wrap',
+            emptyText: 'No images available',
+            renderTo: document.body,
+            width: 230,
+            border: 1,
+            margin: '10 10',
+            style: {
+                borderColor: 'black',
+                borderStyle:'solid'
+            },
+            listeners: {
+
+                itemcontextmenu: function (view, record, item, index, e) {
+                    if (console)
+                        console.log('item context menu');
+
+                    e.stopEvent();
+                    itemContextMenu.showAt(e.getXY());
+                },
+
+                containercontextmenu: function (view, e) {
+                    if (console)
+                        console.log('container context menu');
+
+                    e.stopEvent();
+                    containerContextMenu.showAt(e.getXY());
+                }
+            },
+
+            selModel: {
+                mode: "MULTI",
+                ignoreRightMouseSelection: true,
+                deselectOnContainerClick: true
+            }
+
+
+        });
+
+        Ext.util.Observable.capture(view, function (evname) { console.log(evname, arguments); });
+
+        //Ext.Component({
+        //    autoEl: 'form',
+        //    renderTo: document.body
+        //});
+
+        Ext.Component({ 
+            renderTo: document.body,
+
+            tpl: '{name} is {age} years old and lives in {location}', 
+
+            data: { 
+                age: 25, 
+                location: 'Italy', 
+                name: 'Mario' 
+            } 
+        });
+
+        var summary = new Ext.Component({ 
+            renderTo: document.body,
+
+            tpl: '{name} is {age} years old and lives in {location}', 
+
+            data: { 
+                age: 29, 
+                location: 'Italy', 
+                name: 'Mario' 
+            } 
+        }); 
+
+       var button= new Ext.button.Button({ 
+            renderTo: document.body,
+
+            handler: function() { 
+                summary.update({ 
+                    age: 78, 
+                    location: 'Japan', 
+                    name: 'Aimee' 
+                }); 
+            } 
+        });
+
+
+
+       var cmp= new Ext.Component({ 
+
+           renderTo: document.body,
+           autoEl: 'div' , 
+           data: ['London', 'Paris', 'Moscow', 'New York', 'Tokyo'], 
+
+           tpl: [ 
+                   '<tpl for=".">', 
+                       '<li>{.}</li>', 
+                   '</tpl>' 
+           ] 
+       });
+
+       var toogleClass= new Ext.Component({ 
+           renderTo:document.body,
+           autoEl: 'div', 
+           data: ['London', 'Paris', 'Moscow', 'New York', 'Tokyo'], 
+
+           listeners: { 
+               // Add the listener to the component's main el 
+               el: { 
+                   // Use a CSS class to filter the propagated clicks 
+                   delegate: '.list-row', 
+
+                   click: function(ev, li) { 
+                       // Toggle a CSS class on the li when it is clicked 
+                       Ext.fly(li).toggleCls('list-row-selected'); 
+                   } 
+               } 
+           }, 
+
+           tpl: [ 
+                   '<tpl for=".">', 
+                       '<li class="list-row">{.}</li>', 
+                   '</tpl>' 
+           ] 
+       });
+
+       Ext.define('TitledComponent', {
+           extend: 'Ext.Component',
+           renderTo:document.body,
+           baseCls: 'titled-component',
+           childEls: ['body', 'headerEl'],
+
+           renderTpl: [
+               '<h4 id="{id}-headerEl" class="{baseCls}-header">{header:htmlEncode}</h4>',
+               '<div id="{id}-body" class="{baseCls}-body">{% this.renderContent(out, values) %}</div>'
+           ],
+
+           getTargetEl: function () {
+               return this.body;
+           },
+
+           // Override the default implementation to add in the header text 
+           initRenderData: function () {
+               var data = this.callParent();
+
+               // Add the header property to the renderData 
+               data.header = this.header;
+
+               return data;
+           },
+
+           setHeader: function (header) {
+               this.header = header;
+
+               // The headerEl will only exist after rendering 
+               if (this.headerEl) {
+                   this.headerEl.update(Ext.util.Format.htmlEncode(header));
+               }
+           }
+       });
+
+       Ext.define('BiographyComponent', { 
+           extend: 'TitledComponent', 
+           xtype: 'biography', 
+ 
+           header: 'Biography', 
+           tpl: '{name} is {age:plural("year")} old and lives in {location}', 
+ 
+           // Override update to automatically set the date in the header 
+           update: function(data) { 
+               this.callParent(arguments); 
+ 
+               this.setHeader('Biography updated at ' ); 
+           } 
+       });
+        
+        var summary2 = new Ext.create('BiographyComponent',{
+           data: {
+               age: 26,
+               location: 'Italy',
+               name: 'Mario'
+           }
+       });
+
+
+
+
+       Ext.util.Observable.capture(cmp, function (evname) { console.log(evname, arguments); });
+
+        Ext.util.Observable.capture( Ext.Component({ 
+            renderTo:document.body
+        }), function (evname) { console.log(evname, arguments); });
+
+    };
+
+    var comboBoxes = function() {
+        
+        Ext.create('Ext.form.field.ComboBox', {
+            renderTo:document.body,
+            store: ['Red', 'Yellow', 'Green', 'Brown', 'Blue', 'Pink', 'Black']
+        });
+
+
+    };
+
+    comboBoxes();
 });
