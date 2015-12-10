004db85d478143942412c0c10d640691b4528a61
unknown <dauh@SWMINDDAUH2.swmind.lan>
2015-12-10 11:11:51 +0100

    fix

--- Scripts/ext34/components/ext34gridView.js
+++ Scripts/ext34/components/ext34gridView.js
@@ -4,8 +4,21 @@
  * licensing@sencha.com
  * http://www.sencha.com/license
  */
-Ext.onReady(function () {
+Ext.onReady(function () {    
 
-    
+
+    var panel = new Ext.Panel({
+        id: 'images-view',
+        frame: true,
+        width: 535,
+        autoHeight: true,
+        collapsible: true,
+        layout: 'fit',
+        title: 'Simple DataView (0 items selected)',
+        
+        items:[{
+            text:'Hello'
+        }]
+    });
 
 });
