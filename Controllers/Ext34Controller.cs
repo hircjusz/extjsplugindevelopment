bd5df244eea5c1573b13aa1472839b7eb994e2a7
unknown <dauh@SWMINDDAUH2.swmind.lan>
2015-12-10 10:26:42 +0100

    ext/js 3.4

---
+++ Controllers/Ext34Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExtJsPlugins.Controllers
+{
+    public class Ext34Controller : Controller
+    {
+        //
+        // GET: /Ext34/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GridEditors()
+        {
+            return View();
+        }
+
+    }
+}
